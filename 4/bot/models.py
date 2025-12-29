975467e490db5186eb57cf83b2ee92349bee03dc
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-30 03:27:43 +0330

    add types for photo and documents and not texts

--- 4/bot/models.py
+++ 4/bot/models.py
@@ -9,10 +9,18 @@ class TelegramUser(models.Model):
         return f"{self.username or self.first_name} ({self.user_id})"
 
 class Message(models.Model):
+	FILE_TYPE_CHOICES = [
+        ('text', 'Text'),
+        ('photo', 'Photo'),
+        ('document', 'Document'),
+    ]
+
 	user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE, related_name='messages')
 
 	text = models.TextField()
-	message_id = models.IntegerField()	# for later reply
+	message_id = models.IntegerField(blank=True, null=True)
+
+	types = models.CharField(max_length=10, choices=FILE_TYPE_CHOICES, default='text')
 
 	created_at = models.DateTimeField(auto_now_add=True)
 	is_bot = models.BooleanField(default=False)
@@ -22,5 +30,6 @@ class Message(models.Model):
 
 	def __str__(self):
 		sender = "BOT" if self.is_bot else "USER"
-		return f"[{sender}] ({self.user.user_id}): {self.text[:50]}"
+		content = self.text if self.types == 'text' else f"{self.types}({self.message_id})"
+		return f"[{sender}] ({self.user.user_id}): {content}"
           
