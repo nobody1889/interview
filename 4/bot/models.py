f1f7b098132288a5b22bcbeac00bc28110fcc886
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-30 02:57:05 +0330

    add telegram user to save users info too

--- 4/bot/models.py
+++ 4/bot/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
+class TelegramUser(models.Model):
+    user_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.username or self.first_name} ({self.user_id})"
+
 class Message(models.Models):
 	STATUS_CHOICES=[
 		('sender', 'Sender'),
 		('receiver', 'Receiver')
 	]
+
+	user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE, related_name='messages')
+
 	text = models.TextField()
 	message_id = models.IntegerField()	# for later reply
 
