1b65acc467eb73e1355a7f8e9d12c0e245ebb2b0
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-30 02:47:33 +0330

    add ordering for seeing messages in order

--- 4/bot/models.py
+++ 4/bot/models.py
@@ -5,12 +5,14 @@ class Message(models.Models):
 		('sender', 'Sender'),
 		('receiver', 'Receiver')
 	]
-	
 	text = models.TextField()
 	message_id = models.IntegerField()	# for later reply
 
 	create_at = models.DateTimeField(auto_now_add=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+	class Meta:
+		ordering = ['-created_at']
 
 	def __str__(self):
 		return f"[{self.status}]: \n\t[{self.text}]"
