4ae138cf349f5d52905e333a34936cc887d52706
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-30 03:09:07 +0330

    create admin panel for users and messages

--- 4/bot/admin.py
+++ 4/bot/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TelegramUser, Message
 
-# Register your models here.
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name')
+    search_fields = ('username', 'first_name', 'user_id')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'created_at')
+    search_fields = ('text',)
