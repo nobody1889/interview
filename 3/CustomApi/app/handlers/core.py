7189b4ef3ccafc42eb3e414cdca62c69021cc032
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-30 04:15:51 +0330

    solving typo

--- 3/CustomApi/app/handlers/core.py
+++ 3/CustomApi/app/handlers/core.py
@@ -1,5 +1,5 @@
-from command.start import start_command
-from command.help import help_command
+from commands.start import start_command
+from commands.help import help_command
 from feature.echo import echo_feature
 
 async def core(api, updates):
@@ -16,10 +16,13 @@ async def core(api, updates):
 
         if text.startswith("/"):
             command = text.split()[0]  
+
             if command == "/start":
                 await start_command(api, chat_id)
+
             elif command == "/help":
                 await help_command(api, chat_id)
+
             else:
                 await api.send_message(chat_id, f"Unknown command: {command}")
         
