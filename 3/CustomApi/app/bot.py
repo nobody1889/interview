135de05b15859b98f97c1dd4162de8bdeed17e60
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-30 04:44:22 +0330

    solve syntacs error

--- 3/CustomApi/app/bot.py
+++ 3/CustomApi/app/bot.py
@@ -1,6 +1,7 @@
 from .utils.telegram_api import TelegtamApi
-from handlers.core import core
+from .handlers.core import core
 import asyncio
+
 class Bot:
     def __init__(self):
         self.api = TelegtamApi()
@@ -10,7 +11,3 @@ class Bot:
         while True:
             updates = await self.api.get_updates(self.offset)
             await core(self.api, updates)
-
-if __name__ == "__main__":
-    bot = Bot()
-    asyncio.run(bot.run())
